71f9ddf0b50011d6ea9e343623b3ec2f869a3c13
Duy Hoàng <duypo03cbvn@gmail.com>
2025-10-11 09:31:44 +0700

    Fix relative import bug

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,7 +2,7 @@ from .distance import *
 from .loss_functions import *
 from .preprocess import *
 # Export training helpers explicitly to avoid circular imports when modules import `utils`
-from .preprocess import sample_image
+from .preprocess import load_data
 from .distance import gradient_distance
 from .augment import DiffAugment, ParamDiffAug
-from .common import evaluate_dii_method, evaluate_dim_method, generate_sample_dim, train_acgan, train_cgan, get_images
+from .common import evaluate_dii_method, evaluate_dim_method, generate_sample_dim, train_cgan, get_images
