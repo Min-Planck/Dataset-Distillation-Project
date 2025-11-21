0ecdf2ecb7810f5e9d1e175f56108c7d817970e1
Duy Hoàng <duypo03cbvn@gmail.com>
2025-11-21 15:00:02 +0700

    change config

--- src/gan/__init__.py
+++ src/gan/__init__.py
@@ -1,4 +1,4 @@
 from .acgan import get_acgan, train_acgan
 from .cgan import get_cgan, train_cgan
 
-__all__ = ['get_acgan', 'get_cgan']
+__all__ = ['get_acgan', 'get_cgan', 'train_acgan', 'train_cgan']
